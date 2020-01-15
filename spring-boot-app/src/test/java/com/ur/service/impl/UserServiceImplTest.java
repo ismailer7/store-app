db465d992292061ac991853b157bd45276e42249
Ismail <ismailrzouki7@gmail.com>
2020-01-16 00:38:07 +0100

    test the home controller.

--- spring-boot-app/src/test/java/com/ur/service/impl/UserServiceImplTest.java
+++ spring-boot-app/src/test/java/com/ur/service/impl/UserServiceImplTest.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
---
+++ spring-boot-app/src/test/java/com/ur/web/HomeControllerTest.java
@@ -0,0 +1,32 @@
+package com.ur.web;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@ComponentScan(basePackages = {"com.ur"}) 
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HomeControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void testHi() throws Exception {
+		this.mockMvc.perform(get("/hi")).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("Hello over there!")));
+	}
+}
