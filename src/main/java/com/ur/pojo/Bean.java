3eed47dd368c3eed0749f41d7fefed43732ef447
Ismail <ismailrzouki7@gmail.com>
2020-03-08 12:00:38 +0100

    Add Bean for handling post methods

---
+++ src/main/java/com/ur/pojo/Bean.java
@@ -0,0 +1,16 @@
+package com.ur.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Bean {
+
+	private Long userId;
+	
+	private Long storeId;
+	
+}
